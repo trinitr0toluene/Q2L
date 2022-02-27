5ef52ee1ef2eacbb9865ed659ad5a53c8b42c6fa
trinitr0toluene <764908384@qq.com>
2022-02-27 20:51:55 +0800

    update q2l_infer.py

--- q2l_infer.py
+++ q2l_infer.py
@@ -17,8 +17,7 @@ import torch.utils.data
 import torch.utils.data.distributed
 
 import _init_paths
-from dataset.get_dataset import get_datasets
-
+# from dataset.get_dataset import get_datasets
 
 from utils.logger import setup_logger
 import models
@@ -28,62 +27,111 @@ from utils.metric import voc_mAP
 from utils.misc import clean_state_dict
 from utils.slconfig import get_raw_dict
 
+from data_utils.get_dataset_new import get_datasets
+from data_utils.metrics import validate_f1
 
 def parser_args():
     available_models = ['Q2L-R101-448', 'Q2L-R101-576', 'Q2L-TResL-448', 'Q2L-TResL_22k-448', 'Q2L-SwinL-384', 'Q2L-CvT_w24-384']
 
     parser = argparse.ArgumentParser(description='Query2Label for multilabel classification')
-    parser.add_argument('--dataname', help='dataname', default='coco14', choices=['coco14'])
-    parser.add_argument('--dataset_dir', help='dir of dataset', default='/comp_robot/liushilong/data/COCO14/')
-    
-    parser.add_argument('--img_size', default=448, type=int,
-                        help='image size. default(448)')
-    parser.add_argument('-a', '--arch', metavar='ARCH', default='Q2L-R101-448',
-                        choices=available_models,
-                        help='model architecture: ' +
-                            ' | '.join(available_models) +
-                            ' (default: Q2L-R101-448)')
-    parser.add_argument('--config', type=str, help='config file')
+    parser.add_argument('--dataname', help='dataname', default='intentonomy', choices=['coco14','intentonomy'])
+    # YOUR_PATH/DataSet/Intentonomy 
+    parser.add_argument('--dataset_dir', help='dir of dataset', default='/data/zzy_data/intent_resize')
+    parser.add_argument('--img_size', default=224, type=int,
+                        help='size of input images')
 
-    parser.add_argument('--output', metavar='DIR', 
+    parser.add_argument('--output', metavar='DIR', default='/data/zzy_data/zzy_model/new_model/q2l',
                         help='path to output folder')
     parser.add_argument('--loss', metavar='LOSS', default='asl', 
                         choices=['asl'],
                         help='loss functin')
-    parser.add_argument('--num_class', default=80, type=int,
-                        help="Number of classes.")
-    parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
-                        help='number of data loading workers (default: 8)')
-    parser.add_argument('-b', '--batch-size', default=16, type=int,
-                        metavar='N',
-                        help='mini-batch size (default: 16), this is the total '
-                            'batch size of all GPUs')
-    parser.add_argument('-p', '--print-freq', default=10, type=int,
-                        metavar='N', help='print frequency (default: 10)')
-    parser.add_argument('--resume', type=str, metavar='PATH',
-                        help='path to latest checkpoint (default: none)')
-
+    parser.add_argument('--num_class', default=28, type=int,
+                        help="Number of query slots")
     parser.add_argument('--pretrained', dest='pretrained', action='store_true',
                         help='use pre-trained model. default is False. ')
+    parser.add_argument('--optim', default='AdamW', type=str, choices=['AdamW', 'Adam_twd'],
+                        help='which optim to use')
+    parser.add_argument('-a', '--arch', metavar='ARCH', default='Q2L-R101-448',
+                        choices=available_models,
+                        help='model architecture: ' +' | '.join(available_models) +
+                            ' (default: Q2L-R101-448)')
 
+    parser.add_argument('--amp', action='store_true',
+                        help='use mixture precision.')
+
+    # loss
     parser.add_argument('--eps', default=1e-5, type=float,
-                    help='eps for focal loss (default: 1e-5)')
+                        help='eps for focal loss (default: 1e-5)')
+    parser.add_argument('--dtgfl', action='store_true', default=False, 
+                        help='disable_torch_grad_focal_loss in asl')              
+    parser.add_argument('--gamma_pos', default=0, type=float,
+                        metavar='gamma_pos', help='gamma pos for simplified asl loss')
+    parser.add_argument('--gamma_neg', default=2, type=float,
+                        metavar='gamma_neg', help='gamma neg for simplified asl loss')
+    parser.add_argument('--loss_dev', default=-1, type=float,
+                                            help='scale factor for loss')
+    parser.add_argument('--loss_clip', default=0.0, type=float,
+                                            help='scale factor for clip')  
+
+    parser.add_argument('-j', '--workers', default=32, type=int, metavar='N',
+                        help='number of data loading workers (default: 32)')
+    parser.add_argument('--epochs', default=80, type=int, metavar='N',
+                        help='number of total epochs to run')
+
+    parser.add_argument('--val_interval', default=1, type=int, metavar='N',
+                        help='interval of validation')
+
+    parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                        help='manual epoch number (useful on restarts)')
+    parser.add_argument('-b', '--batch-size', default=128, type=int,
+                        metavar='N',
+                        help='mini-batch size (default: 256), this is the total '
+                            'batch size of all GPUs')
+
+    parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float,
+                        metavar='LR', help='initial learning rate', dest='lr')
+    parser.add_argument('--wd', '--weight-decay', default=1e-2, type=float,
+                        metavar='W', help='weight decay (default: 1e-2)',
+                        dest='weight_decay')
+
+    parser.add_argument('-p', '--print-freq', default=10, type=int,
+                        metavar='N', help='print frequency (default: 10)')
+    parser.add_argument('--resume', default='/data/zzy_data/zzy_model/new_model/q2l/model_best.pth.tar', type=str, metavar='PATH',
+                        help='path to latest checkpoint (default: none)')
+    parser.add_argument('--resume_omit', default=[], type=str, nargs='*')
+    parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                        help='evaluate model on validation set')
+
+    parser.add_argument('--ema-decay', default=0.9997, type=float, metavar='M',
+                        help='decay of model ema')
+    parser.add_argument('--ema-epoch', default=0, type=int, metavar='M',
+                        help='start ema epoch')
+
 
     # distribution training
-    parser.add_argument('--world-size', default=-1, type=int,
+    parser.add_argument('--world-size', default=1, type=int,
                         help='number of nodes for distributed training')
-    parser.add_argument('--rank', default=-1, type=int,
+    parser.add_argument('--rank', default=0, type=int,
                         help='node rank for distributed training')
-    parser.add_argument('--dist-url', default='tcp://127.0.0.1:3451', type=str,
+    parser.add_argument('--dist-url', default='env://', type=str,
                         help='url used to set up distributed training')
     parser.add_argument('--seed', default=None, type=int,
                         help='seed for initializing training. ')
     parser.add_argument("--local_rank", type=int, help='local rank for DistributedDataParallel')
-    parser.add_argument('--amp', action='store_true',
-                        help='use mixture precision.')
+
+
     # data aug
+    parser.add_argument('--cutout', action='store_true', default=False,
+                        help='apply cutout')
+    parser.add_argument('--n_holes', type=int, default=1,
+                        help='number of holes to cut out from image')              
+    parser.add_argument('--length', type=int, default=-1,
+                        help='length of the holes. suggest to use default setting -1.')
+    parser.add_argument('--cut_fact', type=float, default=0.5,
+                        help='mutual exclusion with length. ') 
+
     parser.add_argument('--orid_norm', action='store_true', default=False,
-                        help='using oridinary norm of [0,0,0] and [1,1,1] for mean and std.')
+                        help='using mean [0,0,0] and std [1,1,1] to normalize input images')
 
 
     # * Transformer
@@ -91,9 +139,9 @@ def parser_args():
                         help="Number of encoding layers in the transformer")
     parser.add_argument('--dec_layers', default=2, type=int,
                         help="Number of decoding layers in the transformer")
-    parser.add_argument('--dim_feedforward', default=256, type=int,
+    parser.add_argument('--dim_feedforward', default=8192, type=int,
                         help="Intermediate size of the feedforward layers in the transformer blocks")
-    parser.add_argument('--hidden_dim', default=128, type=int,
+    parser.add_argument('--hidden_dim', default=2048, type=int,
                         help="Size of the embeddings (dimension of the transformer)")
     parser.add_argument('--dropout', default=0.1, type=float,
                         help="Dropout applied in the transformer")
@@ -110,15 +158,8 @@ def parser_args():
                         help='keep the first self attention module in transformer decoders, which will be removed default.')
     parser.add_argument('--keep_input_proj', action='store_true', 
                         help="keep the input projection layer. Needed when the channel of image features is different from hidden_dim of Transformer layers.")
+
     args = parser.parse_args()
-
-    # update parameters with pre-defined config file
-    if args.config:
-        with open(args.config, 'r') as f:
-            cfg_dict = json.load(f)
-        for k,v in cfg_dict.items():
-            setattr(args, k, v)
-
     return args
 
 def get_args():
@@ -136,6 +177,9 @@ def main():
         # launch by torch.distributed.launch
         # Single node
         #   python -m torch.distributed.launch --nproc_per_node=8 main.py --world-size 1 --rank 0 ...
+        # Multi nodes
+        #   python -m torch.distributed.launch --nproc_per_node=8 main.py --world-size 2 --rank 0 --dist-url 'tcp://IP_OF_NODE0:FREEPORT' ...
+        #   python -m torch.distributed.launch --nproc_per_node=8 main.py --world-size 2 --rank 1 --dist-url 'tcp://IP_OF_NODE0:FREEPORT' ...
         local_world_size = int(os.environ['WORLD_SIZE'])
         args.world_size = args.world_size * local_world_size
         args.rank = args.rank * local_world_size + args.local_rank
@@ -160,15 +204,12 @@ def main():
                                 world_size=args.world_size, rank=args.rank)
     cudnn.benchmark = True
     
-    # set output dir and logger
+     # set output dir and logger
     if not args.output:
         args.output = (f"logs/{args.arch}-{datetime.datetime.now()}").replace(' ', '-')
     os.makedirs(args.output, exist_ok=True)
     logger = setup_logger(output=args.output, distributed_rank=dist.get_rank(), color=False, name="Q2L")
     logger.info("Command: "+' '.join(sys.argv))
-
-
-    # save config to outputdir
     if dist.get_rank() == 0:
         path = os.path.join(args.output, "config.json")
         with open(path, 'w') as f:
@@ -188,12 +229,13 @@ def main_worker(args, logger):
     model = build_q2l(args)
     model = model.cuda()
     model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank], broadcast_buffers=False)
+    # criterion
     criterion = models.aslloss.AsymmetricLossOptimized(
         gamma_neg=args.gamma_neg, gamma_pos=args.gamma_pos,
-        disable_torch_grad_focal_loss=True,
+        disable_torch_grad_focal_loss=args.dtgfl,
         eps=args.eps,
     )
-
+  
 
     # optionally resume from a checkpoint
     if args.resume:
@@ -202,6 +244,8 @@ def main_worker(args, logger):
             checkpoint = torch.load(args.resume, map_location=torch.device(dist.get_rank()))
             state_dict = clean_state_dict(checkpoint['state_dict'])
             model.module.load_state_dict(state_dict, strict=True)
+            # model.module.load_state_dict(checkpoint['state_dict'])
+            #         
             del checkpoint
             del state_dict
             torch.cuda.empty_cache() 
@@ -209,19 +253,23 @@ def main_worker(args, logger):
             logger.info("=> no checkpoint found at '{}'".format(args.resume))
 
     # Data loading code
-    _, val_dataset = get_datasets(args)
-    assert args.batch_size // dist.get_world_size() == args.batch_size / dist.get_world_size(), 'Batch size is not divisible by num of gpus.'
+    train_dataset, val_dataset = get_datasets(args)
+    train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+    assert args.batch_size // dist.get_world_size() == args.batch_size / dist.get_world_size(), 'Batch size is not divisible by num of gpus.' 
     val_sampler = torch.utils.data.distributed.DistributedSampler(val_dataset, shuffle=False)
     val_loader = torch.utils.data.DataLoader(
         val_dataset, batch_size=args.batch_size // dist.get_world_size(), shuffle=False,
         num_workers=args.workers, pin_memory=True, sampler=val_sampler)
 
 
-    # for eval only
-    _, mAP = validate(val_loader, model, criterion, args, logger)
-    logger.info(' * mAP {mAP:.1f}'
-            .format(mAP=mAP))
-    return
+    if args.evaluate:
+        _, mAP, f1_micros, f1_macros, f1_samples = validate(val_loader, model, criterion, args, logger)
+        logger.info(' * mAP {mAP:.5f}'.format(mAP=mAP))
+        logger.info('Validation: f1_micros: {}, f1_macros: {}, f1_samples: {}'.format(f1_micros, f1_macros, f1_samples))
+        
+        return
+    
+
     
 
 
@@ -229,7 +277,10 @@ def main_worker(args, logger):
 def validate(val_loader, model, criterion, args, logger):
     batch_time = AverageMeter('Time', ':5.3f')
     losses = AverageMeter('Loss', ':5.3f')
+    # Acc1 = AverageMeter('Acc@1', ':5.2f')
+    # top5 = AverageMeter('Acc@5', ':5.2f')
     mem = AverageMeter('Mem', ':.0f', val_only=True)
+    # mAP = AverageMeter('mAP', ':5.3f', val_only=)
 
     progress = ProgressMeter(
         len(val_loader),
@@ -237,6 +288,7 @@ def validate(val_loader, model, criterion, args, logger):
         prefix='Test: ')
 
     # switch to evaluate mode
+    
     model.eval()
     saved_data = []
     with torch.no_grad():
@@ -249,14 +301,17 @@ def validate(val_loader, model, criterion, args, logger):
             with torch.cuda.amp.autocast(enabled=args.amp):
                 output = model(images)
                 loss = criterion(output, target)
+               
                 output_sm = nn.functional.sigmoid(output)
+               
 
             # record loss
             losses.update(loss.item(), images.size(0))
             mem.update(torch.cuda.max_memory_allocated() / 1024.0 / 1024.0)
 
-            # save some data
+           
             _item = torch.cat((output_sm.detach().cpu(), target.detach().cpu()), 1)
+          
             saved_data.append(_item)
 
             # measure elapsed time
@@ -273,7 +328,8 @@ def validate(val_loader, model, criterion, args, logger):
             _meter_reduce if dist.get_world_size() > 1 else lambda x: x.avg,
             [losses]
         )
-
+        
+       
         # calculate mAP
         saved_data = torch.cat(saved_data, 0).numpy()
         saved_name = 'saved_data_tmp.{}.txt'.format(dist.get_rank())
@@ -282,24 +338,35 @@ def validate(val_loader, model, criterion, args, logger):
             dist.barrier()
 
         if dist.get_rank() == 0:
-            print("Calculating mAP:")
+            print("Calculating metrics:")
             filenamelist = ['saved_data_tmp.{}.txt'.format(ii) for ii in range(dist.get_world_size())]
             metric_func = voc_mAP                
             mAP, aps = metric_func([os.path.join(args.output, _filename) for _filename in filenamelist], args.num_class, return_each=True)
+            metric_f1 = validate_f1
+            f1_dict = metric_f1([os.path.join(args.output, _filename) for _filename in filenamelist], args.num_class)
+            f1_micros = f1_dict['val_micro']
+            f1_macros = f1_dict['val_macro']
+            f1_samples = f1_dict['val_samples']
             
             logger.info("  mAP: {}".format(mAP))
-            logger.info("   aps: {}".format(np.array2string(aps, precision=5)))
+            logger.info("  aps: {}".format(np.array2string(aps, precision=5)))
+            logger.info("  f1_micros: {}".format(np.array2string(f1_micros, precision=5)))
+            logger.info("  f1_macros: {}".format(np.array2string(f1_macros, precision=5)))
+            logger.info("  f1_samples: {}".format(np.array2string(f1_samples, precision=5)))
+            
         else:
             mAP = 0
+            f1_micros = 0
+            f1_macros = 0
+            f1_samples = 0
 
         if dist.get_world_size() > 1:
             dist.barrier()
 
-    return loss_avg, mAP
+    return loss_avg, mAP, f1_micros, f1_macros, f1_samples
 
 
 ##################################################################################
-
 def _meter_reduce(meter):
     meter_sum = torch.FloatTensor([meter.sum]).cuda()
     meter_count = torch.FloatTensor([meter.count]).cuda()
@@ -362,8 +429,9 @@ class ProgressMeter(object):
         return '[' + fmt + '/' + fmt.format(num_batches) + ']'
 
 
+
 def kill_process(filename:str, holdpid:int) -> List[str]:
-    # used for training only.
+     # used for training only.
     import subprocess, signal
     res = subprocess.check_output("ps aux | grep {} | grep -v grep | awk '{{print $2}}'".format(filename), shell=True, cwd="./")
     res = res.decode('utf-8')
@@ -375,4 +443,13 @@ def kill_process(filename:str, holdpid:int) -> List[str]:
     return idlist
 
 if __name__ == '__main__':
+    # sys.argv = ['main_mlc.py',
+    #     '--dataname','intentonomy',
+    #     # '--evaluate',
+    #     '-b', '32',
+    # ]
+    
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '5678'
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1,2,3,4'
     main()
